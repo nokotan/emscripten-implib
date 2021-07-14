f1a6eda8b5ad1463bacc30e837c1f7df9ff8099d
nokotan <kamenokonokotan@gmail.com>
2021-07-14 20:07:16 +0900

    Update buildtest.sh

--- samples/zlib/buildtest.sh
+++ samples/zlib/buildtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-emcc -c -Os -o main.o main.c -s MAIN_MODULE=2 -I../EmscriptenLibraries/include
-emcc -o main.js -Os main.o libz.stub.a -s MAIN_MODULE=2 -lnodefs.js --pre-js=main.pre.js
+emcc -c -Os -o main.o main.c -s MAIN_MODULE=2 -I../../../EmscriptenLibraries/include
+emcc -o main.js -Os main.o libz.stub.a -s MAIN_MODULE=2 -s EXPORTED_FUNCTIONS="['_main', '_memset', '_memcpy']" -lnodefs.js --pre-js=main.pre.js
